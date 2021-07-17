1c322733123b5c0c800b1a5947a0a4270323431a
Godday Okoduwa <goddayokos@gmail.com>
2021-07-17 13:11:53 +0100

    Experimented with synchronous and asynchronous methods in the AsyncTask class

--- app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
@@ -18,17 +18,27 @@ class MainActivity : AppCompatActivity() {
         mProgressBar = findViewById(R.id.progress_horizontal)
         button = findViewById(R.id.btn)
 
-        button.setOnClickListener { MAsyncTask().execute(10) }
+        button.setOnClickListener {
+//            Synchronous Operations
+//            MAsyncTask().execute(10)
+//            MAsyncTask().execute(10)
+//            MAsyncTask().execute(8)
+//            MAsyncTask().execute(10)
+
+//            Asynchronous operations
+            MAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 10)
+            MAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 10)
+        }
     }
 
     private inner class MAsyncTask : AsyncTask<Int, Int, Void>() {
 
-        override fun doInBackground(vararg params: Int?): Void {
+        override fun doInBackground(vararg params: Int?): Void? {
             for (i in 0..params[0]!!) {
                 publishProgress(i)
                 SystemClock.sleep(500)
             }
-            return Void.TYPE.newInstance()
+            return null
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
