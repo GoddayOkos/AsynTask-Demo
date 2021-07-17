5e43a97119ccb983e1e97b46c8d5e35ef1eba133
Godday Okoduwa <goddayokos@gmail.com>
2021-07-17 14:04:13 +0100

    Handleed memory leaks in AsyncTask

--- app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
@@ -10,39 +10,58 @@ import android.widget.ProgressBar
 class MainActivity : AppCompatActivity() {
     private lateinit var mProgressBar: ProgressBar
     private lateinit var button: Button
+    private var mAsyncTask: MAsyncTask? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAsyncTask = MAsyncTask()
 
         mProgressBar = findViewById(R.id.progress_horizontal)
         button = findViewById(R.id.btn)
 
         button.setOnClickListener {
 //            Synchronous Operations
-//            MAsyncTask().execute(10)
-//            MAsyncTask().execute(10)
-//            MAsyncTask().execute(8)
-//            MAsyncTask().execute(10)
+            mAsyncTask!!.execute(10)
+//            mAsyncTask!!.execute(10)
+//            mAsyncTask!!.execute(8)
+//            mAsyncTask!!.execute(10)
 
 //            Asynchronous operations
-            MAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 10)
-            MAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 10)
+//            MAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 10)
+//            MAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 10)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mAsyncTask != null) mAsyncTask!!.cancel(true)
     }
 
     private inner class MAsyncTask : AsyncTask<Int, Int, Void>() {
 
-        override fun doInBackground(vararg params: Int?): Void? {
-            for (i in 0..params[0]!!) {
-                publishProgress(i)
-                SystemClock.sleep(500)
-            }
-            return null
-        }
+//          This implementation leads to memory leaks
+//        override fun doInBackground(vararg params: Int?): Void? {
+//            for (i in 0..params[0]!!) {
+//                publishProgress(i)
+//                SystemClock.sleep(500)
+//            }
+//            return null
+//        }
 
         override fun onProgressUpdate(vararg values: Int?) {
             mProgressBar.progress = values[0]!!
         }
+
+        override fun doInBackground(vararg params: Int?): Void? {
+            for (i in 0..10) {
+                if (isCancelled) break
+                SystemClock.sleep(1000)
+                onProgressUpdate(i)
+            }
+            return null
+        }
     }
+
 }
