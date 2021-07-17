af44c92963015f3ac9e32cea203e8eaf1de7ba02
Godday Okoduwa <goddayokos@gmail.com>
2021-07-17 12:29:39 +0100

    Connect UI with MainActivity

--- app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
@@ -2,10 +2,18 @@ package dev.decagon.godday.asyntaskandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mProgressBar: ProgressBar
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mProgressBar = findViewById(R.id.progress_horizontal)
+        button = findViewById(R.id.btn)
     }
 }
