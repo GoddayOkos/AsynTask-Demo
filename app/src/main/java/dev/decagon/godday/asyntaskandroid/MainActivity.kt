035887a9435cdff43aa2369b00f38954ea4fcd0c
Godday Okoduwa <goddayokos@gmail.com>
2021-07-17 12:41:18 +0100

    Setup AysncTask, executed work on background thread and updated the UI

--- app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/asyntaskandroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package dev.decagon.godday.asyntaskandroid
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Button
 import android.widget.ProgressBar
 
@@ -15,5 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         mProgressBar = findViewById(R.id.progress_horizontal)
         button = findViewById(R.id.btn)
+
+        button.setOnClickListener { MAsyncTask().execute(10) }
+    }
+
+    private inner class MAsyncTask : AsyncTask<Int, Int, Void>() {
+
+        override fun doInBackground(vararg params: Int?): Void {
+            for (i in 0..params[0]!!) {
+                publishProgress(i)
+                SystemClock.sleep(500)
+            }
+            return Void.TYPE.newInstance()
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            mProgressBar.progress = values[0]!!
+        }
     }
 }
